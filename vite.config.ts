813a9931c858e814a4033d3e9b85a9b013ff75dc
Lars Thorup <lars@zealake.com>
2023-06-30 17:01:12 +0200

    minor: prettier

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   test: {
     coverage: {
-      reporter: ["lcov", "text", "html"]
+      reporter: ["lcov", "text", "html"],
     },
-  }
+  },
 });
